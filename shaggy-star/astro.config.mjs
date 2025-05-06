d36bf3b2fe2e90ef27600fe959255436ebc2a1b1
Lucas Walica <127564832+WALI272@users.noreply.github.com>
2025-05-06 19:43:41 +0200

    asd

--- shaggy-star/astro.config.mjs
+++ shaggy-star/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
-    base: '/',
+    base: 'WebBaratas',
     site: 'https://webbaratasoviedo.es',
     integrations: [tailwind()],
 });
